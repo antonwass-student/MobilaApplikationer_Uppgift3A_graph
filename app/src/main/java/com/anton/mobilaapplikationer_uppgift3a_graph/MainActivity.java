64aeebb1364e8c3d177b6453c2c5ad5406737772
antonwass <anton_wass@hotmail.com>
2016-12-05 14:33:15 +0100

    Latest

--- app/src/main/java/com/anton/mobilaapplikationer_uppgift3a_graph/MainActivity.java
+++ app/src/main/java/com/anton/mobilaapplikationer_uppgift3a_graph/MainActivity.java
@@ -37,11 +37,9 @@ public class MainActivity extends AppCompatActivity {
         Log.d("", sensorPref);
 
         LineGraphSeries<DataPoint> series = new LineGraphSeries<DataPoint>(new DataPoint[]{
-                new DataPoint(0, 1),
-                new DataPoint(1, 5),
-                new DataPoint(2, -4),
+                new DataPoint(1.5, -4),
                 new DataPoint(3, 2),
-                new DataPoint(4, 6)
+                new DataPoint(4, -2)
         });
 
         graph.addSeries(series);
